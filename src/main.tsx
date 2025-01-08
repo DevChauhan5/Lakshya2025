1f0eeda8a45a63cf1b377dec563ba8d2ddd70ac9
Dev Chauhan <devchauhanthedev@gmail.com>
2025-01-09 00:59:56 +0530

    add initial components and configuration for Tailwind CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  { path: "/", element: <App />, children: [] },
+  { path: "about", element: <h1>About</h1>, children: [] },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
